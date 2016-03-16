8a14478cc1126da00debbb78c4b441b761a5e7ae
Hargitai Ferenc <fhargitai@gmail.com>
2016-03-16 05:39:20 +0100

    diverging from remote

--- topic.rb
+++ topic.rb
@@ -1,4 +1,6 @@
 # this is the additional content for new 'topic'  
 require 'topic/new' # the most important step to start with
+require 'branching/local' # branching_local
 
-#some remarks on it
+# some remarks on it
+# branching_local
