a93ec0359ec23e3ad7833cb62c0116ff9d9184d9
Hargitai Ferenc <fhargitai@gmail.com>
2016-03-16 05:49:41 +0100

    diverging further...

--- topic.rb
+++ topic.rb
@@ -1,6 +1,8 @@
 # this is the additional content for new 'topic'  
 require 'topic/new' # the most important step to start with
 require 'topic/remote' # branching (remote)
+require 'branching/remote' #branching (remote)
 
 # some remarks on it
 # branching (remote)
+# some more comments from branching (remote)
